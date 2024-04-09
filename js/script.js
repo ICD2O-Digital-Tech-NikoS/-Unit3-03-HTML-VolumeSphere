1a86b9c8b3dc9aa1473d1c6e761b02d2869a25cf
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-04-09 15:08:05 +0000

    u

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Niko
+// Created on: april 2024
 // This file contains the JS functions for index.html
+
+
+"use strict"
+// function to calculate area
+function calculateVolume() {
+  // declaring variable for hours worked with value set as value inputed by user 
+  let radius = document.getElementById("radius").value;
+
+  // calculates result by multiplying the product of side a, side b, and height by 1/6
+  let volume = (4/3) * Math.PI * radius ** 3;
+  document.getElementById("user-info").innerHTML = "The volume of the sphere is " + volume.toFixed(3) + " cm<sup>3</sup>.";
+
+  return volume;
+}
