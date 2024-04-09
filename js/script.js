5ad06306e5135d61c4459fd54901340fde88d408
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-04-09 20:14:54 +0000

    u

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@
 
 
 "use strict"
-// function to calculate area
+// function to calculate volume
 function calculateVolume() {
-  // declaring variable for hours worked with value set as value inputed by user 
+  // declaring variable for radius where user input is stored
   let radius = document.getElementById("radius").value;
 
-  // calculates result by multiplying the product of side a, side b, and height by 1/6
+  // calculates volume by multiplying 4/3 with pi times radius cubed
   let volume = (4/3) * Math.PI * radius ** 3;
-  document.getElementById("user-info").innerHTML = "The volume of the sphere is " + volume.toFixed(3) + " cm<sup>3</sup>.";
+  document.getElementById("user-info").innerHTML = "The volume of the sphere is " + volume.toFixed(2) + " cm<sup>3</sup>.";
 
   return volume;
 }
